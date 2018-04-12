078494ccbe1e4f46633d13300f18da2fccdbf4bc
luttik <dtluttik@gmail.com>
2018-04-12 14:19:11 +0200

    Added docstrings for the video conversion functions

--- models/video.py
+++ models/video.py
@@ -5,12 +5,13 @@ from methods import get_openpose_output, determine_video_meta_data
 
 
 class Video:
-    def __init__(self,
-                 people_per_frame: List[List[Dict]],
-                 source: str,
-                 frame_rate: int = -1,
-                 width: int = -1,
-                 height: int = -1) -> None:
+    def __init__(
+            self,
+            people_per_frame: List[List[Dict]],
+            source: str,
+            frame_rate: int = -1,
+            width: int = -1,
+            height: int = -1) -> None:
         super().__init__()
 
         self.people_per_frame = people_per_frame
@@ -19,7 +20,14 @@ class Video:
         self.width = width
         self.height = height
 
-    def to_json(self, location: str = None):
+    def to_json(
+            self,
+            location: str = None) -> None:
+        """
+        Serializes to a json file
+
+        :param location: The location to write to.
+        """
         if location is None:
             location = os.path.join("./data/video_data/", self.source + ".json")
 
@@ -30,7 +38,16 @@ class Video:
                    "source": self.source}, open(location, 'w'))
 
     @staticmethod
-    def from_json(relative_file_name: str, folder: str = './data/video_data/') -> 'Video':
+    def from_json(
+            relative_file_name: str,
+            folder: str = './data/video_data/') -> 'Video':
+        """
+        Retreives a serialised Video object
+
+        :param relative_file_name: The file to retrieve from
+        :param folder: The folder to retrieve from.
+        :return: The object that is loaded
+        """
         data = json.load(os.path.join(folder, relative_file_name))
         return Video(
             data['people_per_frame'],
@@ -41,6 +58,12 @@ class Video:
 
     @staticmethod
     def all_from_json(folder_name: str = './data/parsed_movies/') -> List['Video']:
+        """
+        Loads all Video objects stored in folder_name
+
+        :param folder_name: The location to search in
+        :return: A list with all the Videos
+        """
         return [
             Video.from_json(os.path.join(folder_name, file))
             for file in os.listdir(folder_name)
@@ -48,7 +71,17 @@ class Video:
         ]
 
     @staticmethod
-    def from_open_pose_data(openpose_folder: str, video_location: str = None) -> 'Video':
+    def from_open_pose_data(
+            openpose_folder: str,
+            video_location: str = None) -> 'Video':
+        """
+        Reads a folder with open-pose frames, combines those frames into a Video
+
+        :param openpose_folder: The location of the openpose output.
+        :param video_location: The location of the original movie source file
+        :return: The video as described by the openpose frames
+        """
+
         source = ''.join(openpose_folder.split('\\')[-1].split('.')[:-1])
         people_per_frame = get_openpose_output(openpose_folder)
         if video_location:
--- pipelines/convert_openpose.py
+++ pipelines/convert_openpose.py
@@ -1,9 +1,29 @@
+"""
+A pipeline to load all the openpose output
+"""
 import os
 
 from models import Video
 
 if __name__ == '__main__':
-    root = './data/open_pose_output'
-    for folder in os.listdir(root):
-        video = Video.from_open_pose_data(os.path.join(root, folder))
+    # set the local files where the video's and openpose output are stored
+    openpose_root = './data/open_pose_output'
+    video_root = './data/video'
+
+    # iterate over the folders in the openpose root folder (where there is a subfolder per video)
+    for folder in os.listdir(openpose_root):
+
+        # Get the subfolder with openpose data
+        open_pose_folder = os.path.join(openpose_root, folder)
+        # Get the video file
+        video_file = os.path.join(video_root, folder)
+
+        # use locations and parse to Video object
+        if os.path.exists(video_file):
+            video: Video = Video.from_open_pose_data(open_pose_folder, video_file)
+        else:
+            video: Video = Video.from_open_pose_data(open_pose_folder)
+
+        print('finished video: ', folder)
+        # Serialise object for later use
         video.to_json()
