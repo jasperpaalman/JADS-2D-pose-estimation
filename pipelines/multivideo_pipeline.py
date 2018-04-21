9f386cd9941662a690eb6421c7ae96d41d5044c5
DESKTOP-L8BEMS8\herbe <herbertvleeuwen@gmail.com>
2018-04-21 13:51:24 +0200

    merge push

--- pipelines/multivideo_pipeline.py
+++ pipelines/multivideo_pipeline.py
@@ -7,6 +7,11 @@ run_openpose.run_openpose()
 
 # convert naar leesbare data types aka period person devision
 
+
+
+
+
+
 # Convert naar bruikbare data type period_running_person division, alle fragment soorten
 
 # feature extraction speed / variation / stepping freq
