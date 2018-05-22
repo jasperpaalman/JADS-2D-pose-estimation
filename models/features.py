70f75b596ac0fb7d6bbaadc8581b0972eb1eb3a4
Jasper Paalman <jasper_paalman@hotmail.com>
2018-05-22 13:34:18 +0200

    models documentation

--- models/features.py
+++ models/features.py
@@ -26,6 +26,13 @@ class Features:
 
     @staticmethod
     def __get_features(preprocessor: Preprocessor) -> DataFrame:
+        """
+        Given instance of Preprocessor class returns a Dataframe containing all features for this video
+
+        :param preprocessor: Instance of Preprocessor class
+
+        :return: Dataframe containing all features for this video
+        """
         coord_df = Features.get_dataframe_from_coords(
             preprocessor.get_period_person_division(),
             preprocessor.get_running_person_identifiers(),
@@ -53,13 +60,15 @@ class Features:
     @staticmethod
     def get_coord_list(period_person_division, running_person_identifiers, running_fragments):
         """"
-        Returns a list of all coordinates
+        Returns a list of all coordinates for running fragments
 
-        :param period_person_division:
-        :param running_person_identifiers:
-        :param running_fragments:
+        :param period_person_division: Data strucure containing per frame all persons and their corresponding
+                                       coordinates
+        :param running_person_identifiers: The indices of identified 'people' that belong to the running person
+        :param running_fragments: The estimated fragments where the person under observation is running
 
-        :return coord_list:
+        :return coord_list: A list with for each running fragment a dictionary with frame as key and a coordinate
+                            dictionary as value
         """
         coord_list = []
         for n, running_fragment in enumerate(running_fragments):
@@ -76,8 +85,13 @@ class Features:
     @staticmethod
     def angle_between(p1, p2):
         """
-        Calculate the clockwise angle between two points. Image drawing two lines from the origin (0,0) to both points
-        and returning the angle between both in degrees.
+        Calculates the clockwise angle between two points. Imagine drawing two lines from the origin (0,0) to both
+        points and returning the angle between both in degrees.
+
+        :param p1: Point 1
+        :param p2: Point 2
+
+        :return: Angle between the two points in degrees
         """
         ang1 = np.arctan2(*p1[::-1])
         ang2 = np.arctan2(*p2[::-1])
@@ -86,7 +100,14 @@ class Features:
     @staticmethod
     def rotate(point, angle, origin=(0, 0)):
         """
-        Rotate a point counterclockwise by a given angle around a given origin. The angle should be given in degrees.
+        Rotate a point (x,y) counterclockwise by a given angle around a given origin. The angle should be given in
+        degrees.
+
+        :param point: Point as (x,y)
+        :param angle: Rotation angle in degrees
+        :param origin: Origin to rotate around
+
+        :return: Rotated point as (x,y)
         """
         angle = math.radians(angle)
 
@@ -100,8 +121,12 @@ class Features:
     @staticmethod
     def get_rotation_angle(coord_df):
         """
-        Given the coordinate dataframe with all the running coordinates, this function calculates what the degree to
+        Given the coordinate dataframe with all the running coordinates, this function calculates the degree to
         which the video is tilted (trend present). The rotation angle in degrees is returned to allow de-trending.
+
+        :param coord_df: A dataframe containing all relevant coördiantes observed in the video.
+
+        :return: Rotation angle in degrees by which all coordinates should be rotated to detrend the video
         """
 
         running_coords = coord_df[['x', 'y']].as_matrix()
@@ -119,7 +144,7 @@ class Features:
 
         # Move line over to start at (0,0) and get the rotation_angle
         xnew_origin = xnew - x_coords.min()
-        if ynew[0] < ynew[-1]:
+        if ynew[0] < ynew[-1]:  # determine the direction of the line
             ynew_origin = ynew - ynew.min()
             rotation_angle = - Features.angle_between((xnew_origin[-1], ynew_origin[-1]), (xnew_origin[-1], 0))
         else:
@@ -132,6 +157,11 @@ class Features:
     def reject_outliers(data, m=2):
         """
         Given an array of values return a boolean array indicating whether each entry is an outlier or not.
+
+        :param data: Array of data points
+        :param m: Float indicating the number of standard deviations to set as threshold
+
+        :return: Returns a boolean array indicating if a point is not an outlier
         """
         return abs(data - np.mean(data)) < m * np.std(data)
 
@@ -139,6 +169,12 @@ class Features:
     def process_coord_df(coord_df, period_running_person_division):
         """
         Process coord_df by de-trending and removing outliers and normalization.
+
+        :param coord_df: A dataframe containing all relevant coördinates observed in the video
+        :param period_running_person_division: Data strucure containing per frame all running persons and their
+                                            corresponding coordinates
+
+        :return: A dataframe containing all relevant procesed coordinates observed in the video
         """
 
         # Get rotation angle for de-trending
@@ -148,7 +184,7 @@ class Features:
             *coord_df[['x', 'y']].apply(lambda d: Features.rotate((d['x'], d['y']), rotation_angle), axis=1))
         # Remove outliers for each joint
         coord_df = coord_df[coord_df.groupby(['Point'])['y'].transform(Features.reject_outliers).astype(bool)]
-
+        # Get length of person in pixels to normalize coordinate values by
         pixel_length = np.mean(Features.get_person_length_in_pixels(period_running_person_division))
 
         coord_df["x"] = coord_df["x"] / pixel_length
@@ -156,23 +192,21 @@ class Features:
 
         return coord_df
 
-    # To dataframe
     @staticmethod
     def get_dataframe_from_coords(period_person_division, running_person_identifiers, running_fragments):
-        """"
+        """
         Get a list of coordinates and turns this into a DataFrame to be used for analysis
 
-        The for loop when the 'Fragment' = i+1 is done should become a double for loop, also naming the video number,
-        when adding more videos
+        :param period_person_division: Data strucure containing per frame all persons and their corresponding
+                                       coordinates
+        :param running_person_identifiers: The indices of identified 'people' that belong to the running person
+        :param running_fragments: The estimated fragments where the person under observation is running
 
-        :param running_fragments:
-        :param running_person_identifiers:
-        :param period_person_division:
-        :return coord_df: A DataFrame containing all x and y coordinates of the runner during the run.
+        :return: A dataframe containing all relevant coördiantes observed in the video.
         """
         coord_list = Features.get_coord_list(period_person_division, running_person_identifiers, running_fragments)
 
-        # More robust way of creating the coord_df
+        # Create coord_df
         coord_df = pd.DataFrame(
             [(n, frame, ix, *coords) for n, period_dict in enumerate(coord_list) for frame, coord_dict in
              period_dict.items()
@@ -187,6 +221,7 @@ class Features:
         # Turn numerics into names
         coord_df['Point'] = coord_df['Point'].replace(replace_dict)
 
+        # Get period_running_person_division for processing coord_df
         period_running_person_division = {period: {person: coords for person, coords in period_dictionary.items()
                                                    if person in running_person_identifiers}
                                           for period, period_dictionary in period_person_division.items()}
@@ -200,33 +235,36 @@ class Features:
     def forward_leaning_angle(coord_df):
         """
         Create forward leaning feature to be used in classification. The forward leaning feature describes to what
-        extent a person leans forward. which could be an indicator of a good runner
+        extent a person leans forward, which could be an indicator of a good runner
 
         :param coord_df: A dataframe containing all relevant coördiantes observed in the video.
-        :return forward_leaning_per_fragment: Return a list with a forward leaning angle for each fragment
+
+        :return forward_leaning_per_fragment: Return a list with a forward leaning angle for each running fragment
         """
 
         forward_leaning = []
 
         fragments = coord_df['Fragment'].unique()  # get all running fragments
 
-        for fragment in fragments:
-            fragment_df = coord_df[coord_df['Fragment'] == fragment]
+        for fragment in fragments:  # for each running fragment
+            fragment_df = coord_df[coord_df['Fragment'] == fragment]  # make data selection
 
             start = fragment_df[fragment_df['Frame'] == fragment_df['Frame'].min()]['x'].mean()  # start x
             end = fragment_df[fragment_df['Frame'] == fragment_df['Frame'].max()]['x'].mean()  # end x
 
-            forward_leaning.append([])
+            forward_leaning.append([])  # instantiate list for this fragment
 
             frames = fragment_df['Frame'].unique()  # unique frames for this fragment
 
             for frame in frames:
                 df_sel = fragment_df[fragment_df['Frame'] == frame]
                 forward_leaning_angles = []
+                # For the right side as well as the left side derive an angle
                 for points in [('Right Shoulder', 'Right Hip'), ('Left Shoulder', 'Left Hip')]:
+                    # Get x and y coordinates of shoulder and hip points
                     coords = df_sel[df_sel['Point'].isin(points)][['x', 'y']].as_matrix()
-                    if len(coords) == 2:
-                        forward_leaning_point = coords[0] - coords[1]
+                    if len(coords) == 2:  # Only if both the hip and shoulder are present
+                        forward_leaning_point = coords[0] - coords[1]  # shift point to the origin (0,0) by subtraction
 
                         # Determine direction
                         if end > start:  # direction is right
@@ -238,8 +276,11 @@ class Features:
                                                                                  (forward_leaning_point[0],
                                                                                   forward_leaning_point[1])))
                 if len(forward_leaning_angles) > 0:  # If points were found in this frame
+                    # Take mean over left and right forward leaning estimations
                     forward_leaning[fragment].append(np.mean(forward_leaning_angles))
 
+        # For each fragment (multiple frames per fragment) take the median forward leaning angle to limit the
+        # effect of outliers
         forward_leaning_per_fragment = [np.median(forward_leaning_list) for forward_leaning_list in forward_leaning]
 
         return forward_leaning_per_fragment
@@ -248,12 +289,17 @@ class Features:
     def to_feature_df(coord_df, source, period_running_person_division, running_fragments, fragments, fps):
         """
         Gets a DataFrame of coordinates and turns this into features.
-        In this case, the standard deviation of movement vertically. Extension to also horizontally can be easily made
-        in case this helps for discovering speed.
+        In this case, the standard deviation of movement vertically and the forward leaning angle
 
         :param coord_df: A dataframe containing all relevant coördiantes observed in the video.
+        :param source: Name of the video
+        :param period_running_person_division: Data strucure containing per frame all running persons and their
+                                            corresponding coordinates
+        :param running_fragments: The estimated fragments where the person under observation is running
+        :param fragments: The estimated fragments containing start-up, run and slow-down fases
+        :param fps: Frames per second of the shot video
 
-        :return features_df: returns a dataframe containing standard deviations of all observed coordinates
+        :return: A Dataframe with features to apply analysis to
         """
 
         # Set video number
@@ -276,8 +322,14 @@ class Features:
 
     @staticmethod
     def euclidean_pairwise_distance(matrix):
-        """Given a matrix, calculates the pairwise distance between two rows. If the number of rows is not equal to 2
-        NaN is returned"""
+        """
+        Given a matrix, calculates the pairwise distance between two rows. If the number of rows is not equal to 2
+        NaN is returned
+
+        :param matrix: A matrix with data points
+
+        :return: Returns euclidean pairwise distance if matrix contains two rows, else NaN
+        """
 
         if matrix.shape[0] != 2:
             return np.nan
@@ -286,8 +338,17 @@ class Features:
 
     @staticmethod
     def get_person_length_in_pixels(period_running_person_division, joint_confidence=0.5):
-        """Given the provided length of a person and some confidence bound on each joint ('gewricht' in Dutch) returns a
-        measurement of a persons length in pixel values."""
+        """
+        Given the provided length of a person and some confidence bound on each joint ('gewricht' in Dutch) returns a
+        measurement of a persons length in pixel values.
+
+        :param period_running_person_division: Data strucure containing per frame all running persons and their
+                                            corresponding coordinates
+        :param joint_confidence: Confidence level between 0 and 1, which limits the coordinates that are used in
+                                 calculations
+
+        :return: Returns the length of the person under observation in pixels
+        """
 
         # z value in the x,y,z coordinate output. Set a threshold to only include fairly certain coords
 
@@ -318,7 +379,18 @@ class Features:
 
     @staticmethod
     def speed_via_distance(period_running_person_division, running_fragments, fragments, fps, distance=16500):
-        """Returns estimated speed in km/h per running fragment by using provided distance as inference measurement."""
+        """
+        Returns estimated speed in km/h per running fragment by using provided distance as inference measurement.
+
+        :param period_running_person_division: Data strucure containing per frame all running persons and their
+                                            corresponding coordinates
+        :param running_fragments: The estimated fragments where the person under observation is running
+        :param fragments: The estimated fragments containing start-up, run and slow-down fases
+        :param fps: Frames per second of the shot video
+        :param distance: Distance in mm that is travelled each fragment
+
+        :return: An array with for each running fragment an estimated speed in km/h
+        """
 
         distance_in_meters = distance / 1000
 
@@ -357,8 +429,21 @@ class Features:
 
         return speed
 
+    @staticmethod
     def speed_via_length(period_running_person_division, running_fragments, length, fps, joint_confidence=0.5):
-        """Returns estimated speed in km/h per running fragment by using provided length as inference measurement."""
+        """
+        Returns estimated speed in km/h per running fragment by using provided length as inference measurement.
+
+        :param period_running_person_division: Data strucure containing per frame all running persons and their
+                                            corresponding coordinates
+        :param running_fragments: The estimated fragments where the person under observation is running
+        :param length: Length of person in cm
+        :param fps: Frames per second of the shot video
+        :param joint_confidence: Confidence level between 0 and 1, which limits the coordinates that are used in
+                                 calculations
+
+        :return: An array with for each running fragment an estimated speed in km/h
+        """
 
         pixel_length = Features.get_person_length_in_pixels(period_running_person_division, joint_confidence)
         length_in_meters = length / 100
@@ -368,8 +453,8 @@ class Features:
         speed = []
 
         for start, end in running_fragments:
-            start = int(round(start, 0))
-            end = int(round(end, 0))
+            start = min(period_running_person_division.keys(), key=lambda x: abs(x - start))
+            end = min(period_running_person_division.keys(), key=lambda x: abs(x - end))
 
             start_x = np.nanmean(
                 [np.mean(coords[~(coords == 0).any(axis=1)][:, 0]) for coords in
@@ -392,6 +477,16 @@ class Features:
     def get_plottables(period_person_division, running_person_identifiers, running_fragments, turning_fragments):
         """
         Function to construct all plottable files. In principle to be used for visualisation.
+
+        :param period_person_division: Data strucure containing per frame all persons and their corresponding
+                                       coordinates
+        :param running_person_identifiers: The indices of identified 'people' that belong to the running person
+        :param running_fragments: The estimated fragments where the person under observation is running
+        :param turning_fragments: The estimated fragments where the person under observation is turning
+
+        :return: Plottable information for each distinct part in a fragment. The data structury is similar
+                 to the person_period_division since it gives a data structure containing per person all frames
+                 and the coordinates of that person in that frame.
         """
 
         period_running_person_division = {period: {person: coords
